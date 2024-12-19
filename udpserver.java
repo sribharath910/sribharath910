35097141aa93717cccefb3cabf38b110b22f4466
sribharath910 <sribharath54540@gmail.com>
2024-12-19 10:28:34 +0530

    Add files via upload

---
+++ udpserver.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+public class udpserver
+{
+ public static void main(String args[])
+ {
+ DatagramSocket sock = null;
+ try
+ {
+sock = new DatagramSocket();
+ String s;
+ BufferedReader cin = new BufferedReader(new InputStreamReader(System.in)); 
+ InetAddress host = InetAddress.getByName("localhost");
+ while(true)
+ {
+System.out.println("Enter message to send : ");
+ s = (String)cin.readLine();
+ byte[] b = s.getBytes();
+ DatagramPacket dp = new DatagramPacket(b , b.length,host,7799);
+ sock.send(dp);
+}
+ }
+ catch(IOException e)
+ {
+ System.err.println("IOException " + e);
+ }
+ }
+ }
